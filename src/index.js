2a67847c2e2d265d10f0422136e021eaefe084e1
Jefri Vanegas <vjefri@gmail.com>
2016-04-23 15:29:03 -0700

    lecture 31 done

--- src/index.js
+++ src/index.js
@@ -15,8 +15,11 @@ class App extends Component {
       videos: [],
       selectedVideo: null
     }
+    this.videoSearch('surfboards')
+  }
 
-    YTSearch({key: Keys.google, term: 'surfboards'}, (videos) => {
+  videoSearch(term) {
+    YTSearch({key: Keys.google, term: term}, (videos) => {
       this.setState({
           videos: videos,
           selectedVideo: videos[0]
@@ -26,7 +29,7 @@ class App extends Component {
   render() {
     return (
       <div>
-        <SearchBar />
+        <SearchBar onSearchTermChange={term => this.videoSearch(term)}/>
         <VideoDetail video={this.state.selectedVideo} />
         <VideoList
         onVideoSelect={selectedVideo => this.setState({selectedVideo})}

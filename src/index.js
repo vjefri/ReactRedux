2960b95842d90c51a1e677d546d902e597cd1cbb
Jefri Vanegas <vjefri@gmail.com>
2016-04-23 15:45:34 -0700

    section 2 done

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react'
 import ReactDOM from 'react-dom'
 import YTSearch from 'youtube-api-search'
+import _ from 'lodash'
 
 import Keys from './secret/apikey.js'
 import SearchBar from './components/search_bar'
 import VideoList from './components/video_list'
 import VideoDetail from './components/video_detail'
 
+// class components whenever we want to have state
 class App extends Component {
   constructor(props) {
     super(props)
@@ -27,9 +29,11 @@ class App extends Component {
     })
   }
   render() {
+    const videoSearch = _.debounce((term) => {this.videoSearch(term)}, 300)
+
     return (
       <div>
-        <SearchBar onSearchTermChange={term => this.videoSearch(term)}/>
+        <SearchBar onSearchTermChange={videoSearch} />
         <VideoDetail video={this.state.selectedVideo} />
         <VideoList
         onVideoSelect={selectedVideo => this.setState({selectedVideo})}

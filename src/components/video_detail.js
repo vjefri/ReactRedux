3952be674c95a1cb303b69fd2e5c0850373b0c79
Jefri Vanegas <vjefri@gmail.com>
2016-04-23 15:13:37 -0700

    lecture 29 done

--- src/components/video_detail.js
+++ src/components/video_detail.js
@@ -10,7 +10,7 @@ const VideoDetail = ({video}) => {
   return (
     <div className="video-detail col-md-8">
       <div className="embed-responsive embed-responsive-16by9">
-        <iframe src={url} frameborder="0" className="embed-responsive-item"></iframe>
+        <iframe src={url} className="embed-responsive-item"></iframe>
       </div>
       <div className="details">
         <div>{video.snippet.title}</div>

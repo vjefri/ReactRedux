a6b4976af57be6d6a03ebb586ce5461a89d983e1
Jefri Vanegas <vjefri@gmail.com>
2016-04-22 23:42:12 -0700

    lecture 18

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -3,9 +3,21 @@ import React, { Component } from 'react'
 // plan JS object
 // add methods and values
 // class based component
+// only class based components have state, functional do not have state
 class SearchBar extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = { term: '' }
+  }
+
   render() {
-    return <input onChange={event => console.log(event.target.value)}/>
+    return (
+      <div>
+      <input onChange={event => this.setState({ term: event.target.value })}/>
+      Value of the input: {this.state.term}
+      </div>
+    )
   }
 }
 

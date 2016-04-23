d7445b1de6aec1d69b4deab8cb7a977ff9e7ce5b
Jefri Vanegas <vjefri@gmail.com>
2016-04-22 23:59:57 -0700

    lecture 20

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -14,8 +14,10 @@ class SearchBar extends Component {
   render() {
     return (
       <div>
-      <input onChange={event => this.setState({ term: event.target.value })}/>
-      Value of the input: {this.state.term}
+      <input
+        value={this.state.term}
+        onChange={event => this.setState({ term: event.target.value })}/>
+        Value of the input: {this.state.term}
       </div>
     )
   }

783de177ea09b3687e1c99c76464a194155c8b7d
Jefri Vanegas <vjefri@gmail.com>
2016-04-23 00:16:43 -0700

    lecture 24

--- src/index.js
+++ src/index.js
@@ -1,17 +1,34 @@
-import React from 'react'
+import React, { Component } from 'react'
 import ReactDOM from 'react-dom'
-import Keys from './secret/apikey.js'
+import YTSearch from 'youtube-api-search'
 
+import Keys from './secret/apikey.js'
 import SearchBar from './components/search_bar'
+import VideoList from './components/video_list'
 // create a new component // a component is just a function
 // this component should produce some HTML
 // we create a class component
-const App = () => {
-  return (
-    <div>
-      <SearchBar />
-    </div>
-  )
+// parent component must fetch data
+
+
+class App extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = { videos: [] }
+
+    YTSearch({key: Keys.google, term: 'surfboards'}, (videos) => {
+      this.setState({ videos })
+    })
+  }
+  render() {
+    return (
+      <div>
+        <SearchBar />
+        <VideoList videos={this.state.videos}/>
+      </div>
+    )
+  }
 }
 
 // take this components generated HTML and put it on the page

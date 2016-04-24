f2b0620b19efa74711bebf6ed54a446792fed32f
Jefri Vanegas <vjefri@gmail.com>
2016-04-23 17:15:26 -0700

    lecture 38 done

--- src/index.js
+++ src/index.js
@@ -1,53 +1,9 @@
 import React, { Component } from 'react'
 import ReactDOM from 'react-dom'
-import YTSearch from 'youtube-api-search'
 import _ from 'lodash'
 
-import Keys from './secret/apikey.js'
-import SearchBar from './components/search_bar'
-import VideoList from './components/video_list'
-import VideoDetail from './components/video_detail'
-
-// class components whenever we want to have state
-// whenever we change the state, the components and its children re-render
-// compenent level state
-// in redux state is application level
-
 class App extends Component {
-  constructor(props) {
-    super(props)
 
-    this.state = {
-      videos: [],
-      selectedVideo: null
-    }
-    this.videoSearch('surfboards')
-  }
-
-  videoSearch(term) {
-    YTSearch({key: Keys.google, term: term}, (videos) => {
-      this.setState({
-          videos: videos,
-          selectedVideo: videos[0]
-      })
-    })
-  }
-  render() {
-    const videoSearch = _.debounce((term) => {this.videoSearch(term)}, 300)
-
-    return (
-      <div>
-        <SearchBar onSearchTermChange={videoSearch} />
-        <VideoDetail video={this.state.selectedVideo} />
-        <VideoList
-        onVideoSelect={selectedVideo => this.setState({selectedVideo})}
-        videos={this.state.videos}/>
-      </div>
-    )
-  }
 }
 
-// take this components generated HTML and put it on the page
-// NOTE: we must create an instance of a component
-// NOTE: always one component per file
 ReactDOM.render(<App />, document.querySelector('.container'))

86b5df7d876a2d5ea4d2517dcf55fa5240cb7f3f
Jefri Vanegas <vjefri@gmail.com>
2016-04-25 16:40:58 -0700

    lecture 41 done

--- src/constainers/book-list.js
+++ src/constainers/book-list.js
@@ -1,6 +1,7 @@
 import React,  { Component } from 'react'
+import { connect } from 'react-redux'
 
-export default class Booklist extends Component {
+class Booklist extends Component {
   renderList() {
     return this.props.books.map((book) => {
       return (
@@ -16,3 +17,12 @@ export default class Booklist extends Component {
     )
   }
 }
+
+
+function mapStateToProps(state) {
+  return {
+    books: state.books
+  }
+}
+
+export default connect(mapStateToProps)(BookList)

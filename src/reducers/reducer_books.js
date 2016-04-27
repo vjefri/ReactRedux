ae7f0f0302ebf25d653456dcdfe9b9f15294274d
Jefri Vanegas <vjefri@gmail.com>
2016-04-27 16:19:18 -0700

    lecture 48 done

--- src/reducers/reducer_books.js
+++ src/reducers/reducer_books.js
@@ -1,8 +1,8 @@
 export default function() {
   return [
-    { title: 'Javascript: The Good Parts' },
-    { title: 'Harry Potter' },
-    { title: 'The Dark Tower' },
-    { title: 'Eloquent Ruby' }
+    { title: 'Javascript: The Good Parts', pages: 109 },
+    { title: 'Harry Potter', pages: 100 },
+    { title: 'The Dark Tower', pages: 60 },
+    { title: 'Eloquent Ruby', pages: 70 }
   ]
 }

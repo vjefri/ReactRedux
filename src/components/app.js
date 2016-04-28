44a9a1c8f9a83d1fe7216f427559d583c17699a9
Jefri Vanegas <vjefri@gmail.com>
2016-04-27 17:39:47 -0700

    starter

--- src/components/app.js
+++ src/components/app.js
@@ -1,16 +1,10 @@
-import React from 'react'
-import { Component } from 'react'
-
-import BookList from '../containers/book-list'
-import BookDetail from '../containers/book-detail'
+import React from 'react';
+import { Component } from 'react';
 
 export default class App extends Component {
   render() {
     return (
-      <div>
-        <BookList />
-        <BookDetail />
-      </div>
-    )
+      <div>React simple starter</div>
+    );
   }
 }
--- src/reducers/reducer_books.js
+++
@@ -1,8 +0,0 @@
-export default function() {
-  return [
-    { title: 'Javascript: The Good Parts', pages: 109 },
-    { title: 'Harry Potter', pages: 100 },
-    { title: 'The Dark Tower', pages: 60 },
-    { title: 'Eloquent Ruby', pages: 70 }
-  ]
-}
